5ad7d26f39fbb26cd2d47486b30504688b3c14bb
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-06-13 12:52:12 +0300

    defined the commands for different menu options

--- lib/cli.py
+++ lib/cli.py
@@ -2,3 +2,12 @@ import sys
 from database import CONN, CURSOR  # Importing database connection objects
 from models import Machine, Part, MaintenanceRecord  # Importing models for database interaction
 
+class CLI:
+    def __init__(self):
+        # Defining the commands for different menu options
+        self.commands = {
+            '1': self.machines_menu,
+            '2': self.parts_menu,
+            '3': self.maintenance_records_menu,
+            'q': self.quit
+        }
