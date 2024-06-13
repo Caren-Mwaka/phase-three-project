5e481ac2b81b5537a8b6548316f428b8819fe9f9
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-06-13 12:33:22 +0300

    imported the classes to init

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,2 +1,5 @@
 # To initialize the models folder as a package I added the "init" file. 
 # This allows Python to recognize models as a module that can be imported.
+from models.machines import Machine
+from models.parts import Part
+from models.maintenance_records import MaintenanceRecord
