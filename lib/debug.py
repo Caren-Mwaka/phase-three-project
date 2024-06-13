eb9dbf10368f6944a7a905344138f5bab109c3a2
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-06-13 12:35:08 +0300

    tested the code for machines.py

--- lib/debug.py
+++ lib/debug.py
@@ -12,3 +12,16 @@ if __name__ == "__main__":
 
     MaintenanceRecord.drop_table()
     MaintenanceRecord.create_table()
+
+  
+    machines_data = [
+        {"name": "Lathe Machine", "type": "Heavy-duty"},
+        {"name": "CNC Milling Machine", "type": "Computer Numerical Control"},
+        {"name": "Drilling Machine", "type": "Bench Type"},
+        {"name": "Grinding Machine", "type": "Surface Grinder"},
+        {"name": "Injection Molding Machine", "type": "Hydraulic"},
+        {"name": "Press Machine", "type": "Mechanical"},
+    ]
+
+    for data in machines_data:
+        machine = Machine.create(data["name"], data["type"])
