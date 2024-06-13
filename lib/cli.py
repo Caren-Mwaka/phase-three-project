5a41b91931f02e5d96ef1dba013c8438d458dfb5
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-06-13 12:50:30 +0300

    imported the necessary files and variables needed for the cli

---
+++ lib/cli.py
@@ -0,0 +1,4 @@
+import sys
+from database import CONN, CURSOR  # Importing database connection objects
+from models import Machine, Part, MaintenanceRecord  # Importing models for database interaction
+
